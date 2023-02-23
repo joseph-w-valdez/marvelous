2d776adb238d25d14f020eac0d1e30d833d1c9bf
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2023-02-23 21:54:00 +0000

    basic styling started and functional

---
+++ client/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Navbar(props) {
+  return (
+    <div className='navbar'>
+      <span className='logo'>Marvelous</span>
+      <div className="profile-icon">Image</div>
+    </div>
+  );
+}
