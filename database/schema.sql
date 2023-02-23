744e4801e0df17f9bfba7711aa823fbf1934efbb
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2023-02-23 17:49:03 +0000

    fixed snake-casing to camel-casing

--- database/schema.sql
+++ database/schema.sql
@@ -9,23 +9,23 @@ create schema "public";
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(255) UNIQUE NOT NULL,
-  email_hash CHAR(64) NOT NULL,
-  password_hash CHAR(60) NOT NULL,
-  profile_picture_url VARCHAR(255)
+  emailHash CHAR(64) NOT NULL,
+  passwordHash CHAR(60) NOT NULL,
+  profilePictureUrl VARCHAR(255)
 );
 
 CREATE TABLE characters (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
-  image_url VARCHAR(255) NOT NULL,
-  comic_appearances INT NOT NULL
+  imageUrl VARCHAR(255) NOT NULL,
+  comicAppearances INT NOT NULL
 );
 
 CREATE TABLE favorites (
-  user_id INT NOT NULL,
-  character_id INT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-  FOREIGN KEY (character_id) REFERENCES characters(id) ON DELETE CASCADE,
-  PRIMARY KEY (user_id, character_id)
+  userId INT NOT NULL,
+  characterId INT NOT NULL,
+  FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (characterId) REFERENCES characters(id) ON DELETE CASCADE,
+  PRIMARY KEY (userId, characterId)
 );
