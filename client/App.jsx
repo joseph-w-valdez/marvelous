554e58f7c0d80764d1b663f6411b700aec857629
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2023-02-24 23:46:15 +0000

    routing set up for Character page

--- client/App.jsx
+++ client/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/character" element={Character} />
+        <Route path="/character" element={<Character/>} />
       </Routes>
     </div>
   );
