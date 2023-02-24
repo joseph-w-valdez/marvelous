661de8f2ce1a56e58d00bb5cebafaaf2841a2eef
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2023-02-24 18:52:46 +0000

    front-end can now access back-end to perform GET request to marvel API

--- client/components/Button.jsx
+++ client/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Button = ({ text }) => {
+const Button = ({ text, onClick }) => {
   return (
-    <button className='text-white bg-[#B13434] font-Poppins w-36 h-9 rounded-lg m-8 cursor-pointer'>{text}</button>
+    <button className='text-white bg-[#B13434] font-Poppins w-36 h-9 rounded-lg m-8 cursor-pointer' onClick={onClick}>{text}</button>
   );
 };
 
--- client/components/Container.jsx
+++ client/components/Container.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
+import axios from 'axios';
 
 const buttonText = 'SEARCH';
 
 const Container = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [error, setError] = useState('');
+
+  const searchHandler = () => {
+    const apiUrl = `http://localhost:3000/marvel/${inputValue}`;
+    axios.get(apiUrl)
+      .then(response => {
+        console.log(response.data);
+        setError('');
+      })
+      .catch(error => {
+        console.error(error);
+        setError('error');
+      });
+  };
+
   return (
     <div className='flex flex-wrap justify-center max-w-96 text-center'>
+      {error && <h1 className='text-red-700 bold'>{error}</h1>}
       <p className='text-white font-Poppins w-full p-3'>
         Search for any Marvel Character to learn more about them!
       </p>
-      <input type="text" placeholder='Iron Man' className='w-72 h-9 rounded px-3 mt-3'/>
+      <input
+        type="text"
+        placeholder='Iron Man'
+        className='w-72 h-9 rounded px-3 mt-3'
+        onChange={e => setInputValue(e.target.value)}
+      />
       <div className='basis-full' />
-      <Button text={buttonText}/>
+      <Button text={buttonText} onClick={searchHandler}/>
     </div>
   );
 };
