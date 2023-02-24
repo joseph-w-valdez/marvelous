31b95660c37eee0f0e9533bbf00fc056fb92225d
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-02-24 12:27:50 +0000

    completed subnavbar

---
+++ client/components/Sub-Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SubNavbar = () => {
+  return (
+    <div className="subNavbar w-full  font-Audiowide">
+      <p className='text-white bold text-lg text-center p-1 bg-gradient-to-r from-[#B13434] to-red-900'>SEARCH</p>
+    </div>
+  );
+};
+
+export default SubNavbar;
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import SubNavbar from '../components/Sub-Navbar';
 
 export default function Home(props) {
   return (
     <div>
       <Navbar />
+      <SubNavbar />
     </div>
   );
 }
