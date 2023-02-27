17006d124f245caf303311ad72daa6f257301ecc
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2023-02-27 18:46:08 +0000

    fixed casing in data and schema database files. added in basic registration functionality with hashed password

--- server/index.js
+++ server/index.js
@@ -6,12 +6,24 @@ const errorMiddleware = require('./error-middleware');
 const axios = require('axios');
 const crypto = require('node:crypto');
 
+const pg = require('pg');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 const app = express();
 
 const API_PUBLIC_KEY = 'b8483fd7fba99cd20a9fefc4e5106f88';
 const API_PRIVATE_KEY = 'c2746ff73c66112e104538fe16622cbb21205d8f';
 
 app.use(staticMiddleware);
+app.use(express.json());
 
 app.get('/marvel/:characterName', (req, res, next) => {
 
@@ -42,6 +54,29 @@ app.get('/marvel/:characterName', (req, res, next) => {
       console.error(error);
       next(error);
     });
+});
+
+app.post('/marvel/sign-up', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(400, 'username and password are required fields');
+  }
+  argon2
+    .hash(password)
+    .then(passwordHash => {
+      const sql = `
+        insert into "users" ("username", "passwordHash")
+        values ($1, $2)
+        returning "id", "username", "createdAt"
+      `;
+      const params = [username, passwordHash];
+      return db.query(sql, params);
+    })
+    .then(result => {
+      const [user] = result.rows;
+      res.status(201).json(user);
+    })
+    .catch(err => next(err));
 });
 
 app.use(errorMiddleware);
