65ab08e381cbe45b3d922912234e716e24778bb8
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2023-02-27 22:43:55 +0000

    added functionality with useRef so that if i click on the document outside of the menu, it hides the menu

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import ProfileMenu from './ProfileMenu';
 
 export default function Navbar() {
-  const [profileMenu, setProfileMenu] = useState('closed');
+  const [profileMenu, setProfileMenu] = useState(false);
+  const profileMenuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = event => {
+      if (profileMenuRef.current && !profileMenuRef.current.contains(event.target)) {
+        setProfileMenu(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [profileMenuRef]);
 
   const profileMenuHandler = () => {
-    profileMenu === 'closed' ? setProfileMenu('open') : setProfileMenu('closed');
+    setProfileMenu(!profileMenu);
   };
 
   return (
@@ -14,13 +29,14 @@ export default function Navbar() {
       <div className='flex items-center'>
         <i className="text-white fa-solid fa-bars ml-3 mr-2" />
         <Link to='/'><span className='logo bg-[#B13434] text-white font-Impact text-4xl pl-1 pr-1'>Marvelous</span></Link>
-
       </div>
-      <i
-        className="profile-icon fa-regular fa-user text-white mr-2 cursor-pointer border-2 rounded-full overflow-hidden text-2xl"
-        onClick={profileMenuHandler}
-      />
-      <ProfileMenu profileMenu={profileMenu} />
+      <div ref={profileMenuRef}>
+        <i
+          className="profile-icon fa-regular fa-user text-white mr-2 cursor-pointer border-2 rounded-full overflow-hidden text-2xl"
+          onClick={profileMenuHandler}
+        />
+        <ProfileMenu profileMenu={profileMenu} />
+      </div>
     </div>
   );
 }
--- client/components/ProfileMenu.jsx
+++ client/components/ProfileMenu.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const ProfileMenu = ({ profileMenu }) => {
+
   return (
     <div className={`
     w-48 h-auto rounded-md absolute right-5 text-white font-Poppins top-9 bg-[#333333]
-    pl-2 pt-1 pb-1 cursor-pointer ${profileMenu === 'closed' ? 'hidden' : 'block'}`
+    pl-2 pt-1 pb-1 cursor-pointer ${profileMenu ? 'block' : 'hidden'}`
 } >
       Sign In
     </div>
