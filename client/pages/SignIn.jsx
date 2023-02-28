b7ff8f5e9518e74cf8bb4fe97ac6c9fdc7be630f
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2023-02-28 00:14:41 +0000

    modified click handler f involving useRef. moved ref to the profile icon so clicking on the menu also closed the menu

--- client/pages/SignIn.jsx
+++ client/pages/SignIn.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../components/Button';
 
 const SignIn = ({ onMount }) => {
-
+  const [usernameInputValue, setUsernameInputValue] = useState('');
+  const [passwordInputvalue, setPasswordInputValue] = useState('');
   onMount();
 
   return (
-    <div className='text-white font-Poppins'>SignIn</div>
+    <div className='text-white mx-7 mt-2 font-Poppins flex flex-wrap justify-center'>
+      <h1 className='text-4xl text-center mb-2'>SIGN IN</h1>
+      <div className='basis-full' />
+      <form className='text-center'>
+        <input
+          type="text"
+          placeholder='Username'
+          className='w-72 h-9 rounded px-3 mt-3'
+          value={usernameInputValue}
+          onChange={(e) => setUsernameInputValue(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder='Password'
+          className='w-72 h-9 rounded px-3 mt-3'
+          value={passwordInputvalue}
+          onChange={(e) => setPasswordInputValue(e.target.value)}
+          required
+        />
+        <div className='basis-full' />
+        <Button text='SIGN IN' type="submit" />
+      </form>
+    </div>
   );
 };
 
