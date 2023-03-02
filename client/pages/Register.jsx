b4e0df6ebcba2c45bcd0416c8583ae0dbad0218a
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-03-02 12:58:45 +0000

    added conditional for rendering form data so it disappears if account creation was successful.

--- client/pages/Register.jsx
+++ client/pages/Register.jsx
@@ -42,25 +42,29 @@ const Register = ({ onMount }) => {
       {errorMessage && <h1 className='text-red-700 bold'>{errorMessage}</h1>}
       {successMessage && <h1 className='text-blue-300 bold'>{successMessage} <a href="/sign-in" className='text-blue-500 underline'>here</a></h1>}
       <div className='basis-full' />
-      <form className='text-center text-black' onSubmit={handleSubmit(onSubmit)}>
-        <InputField name="username" register={register} errors={errors} options={{ placeholder: 'Username', validation: usernameValidation }} />
+      {!successMessage && (
+      <>
+        <form className='text-center text-black' onSubmit={handleSubmit(onSubmit)}>
+          <InputField name="username" register={register} errors={errors} options={{ placeholder: 'Username', validation: usernameValidation }} />
+          <div className='basis-full' />
+          <InputField name="email" register={register} errors={errors} options={{ placeholder: 'Email', validation: emailValidation }} />
+          <div className='basis-full' />
+          <InputField name="emailVerification" register={register} control={control} errors={errors} options={{ type: 'email', placeholder: 'Verify Email', validation: { required: true, validate: (value) => value === watch('email') || 'emails do not match' } }} />
+          <div className='basis-full' />
+          <InputField name="password" register={register} control={control} errors={errors} options={{ type: 'password', placeholder: 'Password', validation: passwordValidation }} />
+          <div className='basis-full' />
+          <InputField name="passwordVerification" register={register} control={control} errors={errors} options={{ type: 'password', placeholder: 'Verify Password', validation: { required: true, validate: (value) => value === watch('password') || 'passwords do not match' } }} />
+          <div className='basis-full mb-3' />
+          <Controller name="file" control={control} rules={{ required: false }} render={({ field: { onChange } }) => (<FileInput onChange={(e) => onChange(e.target.files[0])} />)} />
+          <div className='basis-full' />
+          <Button text='Sign Up' />
+        </form>
         <div className='basis-full' />
-        <InputField name="email" register={register} errors={errors} options={{ placeholder: 'Email', validation: emailValidation }} />
-        <div className='basis-full' />
-        <InputField name="emailVerification" register={register} control={control} errors={errors} options={{ type: 'email', placeholder: 'Verify Email', validation: { required: true, validate: (value) => value === watch('email') || 'emails do not match' } }} />
-        <div className='basis-full' />
-        <InputField name="password" register={register} control={control} errors={errors} options={{ type: 'password', placeholder: 'Password', validation: passwordValidation }} />
-        <div className='basis-full' />
-        <InputField name="passwordVerification" register={register} control={control} errors={errors} options={{ type: 'password', placeholder: 'Verify Password', validation: { required: true, validate: (value) => value === watch('password') || 'passwords do not match' } }} />
-        <div className='basis-full mb-3' />
-        <Controller name="file" control={control} rules={{ required: false }} render={({ field: { onChange } }) => (<FileInput onChange={(e) => onChange(e.target.files[0])} />)} />
-        <div className='basis-full' />
-        <Button text='Sign Up' />
-      </form>
-      <div className='basis-full' />
-      <p className='text-white text-sm'>
-        By creating an account, you agree to our <a href="" className='text-blue-500 underline'>Terms of Use</a> and our <a href="" className='text-blue-500 underline'> Privacy Policy</a>
-      </p>
+        <p className='text-white text-sm'>
+          By creating an account, you agree to our <a href="" className='text-blue-500 underline'>Terms of Use</a> and our <a href="" className='text-blue-500 underline'> Privacy Policy</a>
+        </p>
+      </>)
+      }
     </div>
   );
 };
