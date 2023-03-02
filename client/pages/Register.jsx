cb045d2b7fccc1385e12f23253a623c4ea13d313
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-03-02 12:28:02 +0000

    separated important validation rules to separate external file. left the verification validation in main file to watch the form data

--- client/pages/Register.jsx
+++ client/pages/Register.jsx
@@ -5,44 +5,13 @@ import Button from '../components/Button';
 import InputField from '../components/InputField';
 import FileInput from '../components/FileInput';
 import handleRegistration from '../components/handleRegistration';
+import { usernameValidation, emailValidation, passwordValidation } from '../components/validation';
 
 const Register = ({ onMount }) => {
   const [errorMessage, setErrorMessage] = useState(undefined);
   const { control, register, handleSubmit, watch, formState: { errors } } = useForm();
 
   onMount();
-
-  const usernameValidation = {
-    required: true,
-    maxLength: { value: 20, message: 'username cannot be more than 20 characters' }
-  };
-
-  const emailValidation = {
-    required: true,
-    pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: 'must be a valid email address' }
-  };
-
-  const emailVerificationValidation = {
-    required: true,
-    validate: (value) => value === watch('email') || 'emails do not match'
-  };
-
-  const passwordValidation = {
-    required: true,
-    minLength: { value: 8, message: 'must be at least 8 characters long' },
-    maxLength: { value: 25, message: 'cannot be more than 25 characters long' },
-    validate: {
-      uppercase: (value) => /(?=.*[A-Z])/.test(value) || 'must contain at least one uppercase letter',
-      lowercase: (value) => /(?=.*[a-z])/.test(value) || 'must contain at least one lowercase letter',
-      number: (value) => /(?=.*\d)/.test(value) || 'must contain at least one number',
-      special: (value) => /(?=.*[@#$%^&+=!])/.test(value) || 'must contain at least one special character'
-    }
-  };
-
-  const passwordVerificationValidation = {
-    required: true,
-    validate: (value) => value === watch('password') || 'passwords do not match'
-  };
 
   return (
     <div className='text-white mx-7 mt-2 font-Poppins flex flex-wrap justify-center'>
@@ -55,11 +24,11 @@ const Register = ({ onMount }) => {
         <div className='basis-full' />
         <InputField name="email" register={register} errors={errors} options={{ placeholder: 'Email', validation: emailValidation }} />
         <div className='basis-full' />
-        <InputField name="emailVerification" register={register} control={control} errors={errors} options={{ type: 'email', placeholder: 'Verify Email', validation: emailVerificationValidation }} />
+        <InputField name="emailVerification" register={register} control={control} errors={errors} options={{ type: 'email', placeholder: 'Verify Email', validation: { required: true, validate: (value) => value === watch('email') || 'emails do not match' } }} />
         <div className='basis-full' />
         <InputField name="password" register={register} control={control} errors={errors} options={{ type: 'password', placeholder: 'Password', validation: passwordValidation }} />
         <div className='basis-full' />
-        <InputField name="passwordVerification" register={register} control={control} errors={errors} options={{ type: 'password', placeholder: 'Verify Password', validation: passwordVerificationValidation }} />
+        <InputField name="passwordVerification" register={register} control={control} errors={errors} options={{ type: 'password', placeholder: 'Verify Password', validation: { required: true, validate: (value) => value === watch('password') || 'passwords do not match' } }} />
         <div className='basis-full mb-3' />
         <Controller name="file" control={control} rules={{ required: false }} render={({ field: { onChange } }) => (<FileInput onChange={(e) => onChange(e.target.files[0])} />)} />
         <div className='basis-full' />
