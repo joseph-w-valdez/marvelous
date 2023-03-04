c1a0837e0554c295d31b38b968b7805eab9fc126
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-03-04 23:46:23 +0000

    fixed the subnavbar to have fixed positioning

--- client/components/SubNavbar.jsx
+++ client/components/SubNavbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SubNavbar = ({ text }) => {
   return (
-    <div className="subNavbar w-full  font-Audiowide">
+    <div className="subNavbar w-full font-Audiowide fixed">
       <p className='text-white bold text-lg text-center p-1 bg-gradient-to-r from-[#B13434] to-red-900'>{text}</p>
     </div>
   );
