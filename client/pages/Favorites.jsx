2936c4c4727eeaa25f41714b5eebb3c1a5714031
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-03-06 05:40:26 +0000

    added conditional rendering to display message for if no favorites exist along with Link to the home page to search

--- client/pages/Favorites.jsx
+++ client/pages/Favorites.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useUser } from '../contexts/UserContext';
 import axiosPost from '../utils/AxiosPost';
 import Button from '../components/Button';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Favorites = ({ onMount }) => {
   const { user, setUser } = useUser();
@@ -27,21 +28,28 @@ const Favorites = ({ onMount }) => {
   return (
     <div className='text-white mx-7 mt-2 font-Poppins flex flex-wrap justify-center'>
       <h1 className='text-4xl mb-4 w-full text-center'>Favorite Characters:</h1>
-      {characters.map((character) => (
-        <div key={character.id} className='flex flex-wrap w-full justify-center mb-4'>
-          <div className="max-w-sm">
-            <img
-              src={character.imageUrl}
-              alt={character.name}
-              className='border-18 border-red-border w-full object-contain'
-              style={{ borderImage: 'linear-gradient(to bottom, #B13434, #7F1D1D) 1' }}
-            />
-            <h1 className='text-3xl mt-2'>{character.name}</h1>
-            <Button text='VIEW PROFILE'/>
+      {characters.length === 0
+        ? (
+          <div className="w-full text-center text-2xl">
+            No favorites found! Click <Link to='/' className='text-blue-500 underline'>here</Link> to search for characters to add!
           </div>
-        </div>
-      ))}
-
+          )
+        : (
+            characters.map((character) => (
+              <div key={character.id} className='flex flex-wrap w-full justify-center mb-4'>
+                <div className="max-w-sm">
+                  <img
+                src={character.imageUrl}
+                alt={character.name}
+                className='border-18 border-red-border w-full object-contain'
+                style={{ borderImage: 'linear-gradient(to bottom, #B13434, #7F1D1D) 1' }}
+              />
+                  <h1 className='text-3xl mt-2'>{character.name}</h1>
+                  <Button text='VIEW PROFILE' />
+                </div>
+              </div>
+            ))
+          )}
     </div>
   );
 };
